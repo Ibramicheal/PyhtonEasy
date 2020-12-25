57085cecbdf2195ac9eaa72d151468efcec48671
Ibramicheal <63258673+Ibramicheal@users.noreply.github.com>
2020-12-25 16:55:38 +0000

    final

--- main.py
+++ main.py
@@ -14,6 +14,15 @@ cost = 2.50
 yearOfRelease = 2018
 monthOfRelease = "september"
 dayOfRelease = 24
-viewsOnYouTube = 31,071,275
+viewsOnYouTube = 31071275
 
-print(Artist,Title,Genre,DurationInSeconds,fileType,cost,yearOfRelease,monthOfRelease,dayOfRelease,viewsOnYouTube)
+print("Artist: {}".format(Artist))
+print("Title: {}".format(Title))
+print("Genre: {}".format(Genre))
+print("Duration: {}".format(DurationInSeconds))
+print("File: {}".format(fileType))
+print("Day of Release: {} ".format(dayOfRelease))
+print("Month of Release: {}".format(monthOfRelease))
+print("Year of Release: {}".format(yearOfRelease))
+print("Number of Views on YouTube: {}".format(viewsOnYouTube))
+print("Cost per song: {}".format(cost))
