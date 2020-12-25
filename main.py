3906b37728b7d705dd37ffad5f9b8e6f5ff4957b
Ibramicheal <63258673+Ibramicheal@users.noreply.github.com>
2020-12-25 16:23:34 +0000

    Test

---
+++ main.py
@@ -0,0 +1,17 @@
+#A prog for my favourite song
+"""
+printing the details of my favourite song, hope you enjoy the song.
+This is my gift to you.
+"""
+
+Artist = "Mbosso "
+Title = "Hodari"
+Genre = "Bongo flavour"#a type of tanzanian music
+DurationInSeconds = "4mins 02secs"
+fileType = "mp4"
+#Cost in pounds
+cost = 2.50
+yearOfRelease = 2018
+monthOfRelease = "september"
+dayOfRelease = 24
+viewsOnYouTube = 31,071,275
