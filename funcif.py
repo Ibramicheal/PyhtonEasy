5ad0f642529e307524f12564501fe0b383346717
Ibramicheal <63258673+Ibramicheal@users.noreply.github.com>
2020-12-27 19:46:33 +0000

    final

--- funcif.py
+++ funcif.py
@@ -1,12 +1,13 @@
 # creating a function that takes in 3 parameters and check if they are equal to eacother
 
-#def ConditionalCheck(num1,num2,num3):
-if num1 == num2 and num2 == num3:
+def ConditionalCheck(num1,num2,num3):
+    if num1 == num2 and num2 == num3:
+        print(True)
+    elif num2 == num1 or num1 == num3:
+        print(True)
+    elif num1 == int(num3) or num2 < num1:
+        print(True)
+    else:
+        print(False)
 
-    print(True)
-elif num2 <= num1 or num1 == num3:
-
-    print(True)
-else:
-
-    print(False)
+ConditionalCheck(3,2,"3")
